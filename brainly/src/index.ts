0b2443cf9394961cce3c52443d0d1aaaeeb11972
gaurav5xy <gauravkumar81464@gmail.com>
2025-01-21 23:25:19 +0530

    content and middel

--- brainly/src/index.ts
+++ brainly/src/index.ts
@@ -5,12 +5,16 @@ import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt";
 import "dotenv/config";
 import { z } from 'zod';
-import { UserModel } from "../db/schema";
+import { ContentModel, TagtModel, UserModel } from "../db/schema";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+interface JwtPayload {
+    userId: string
+}
 
 app.post("/signup", async (req: Request, res: Response): Promise<any> => {
     const requiredBody = z.object({
@@ -38,7 +42,7 @@ app.post("/signup", async (req: Request, res: Response): Promise<any> => {
         res.status(200).json({ message: "You are registered successfully" });
     } catch (error) {
         console.error("Server-side error:", error);
-        res.status(500).json({ message: "Server-side error", error});
+        res.status(500).json({ message: "Server-side error", error });
     }
 });
 
@@ -55,42 +59,108 @@ app.post('/signin', async (req: Request, res: Response): Promise<any> => {
 
     const { username, password } = parsedBody.data;
     try {
-        const findUser = await UserModel.findOne({username: username});
+        const findUser = await UserModel.findOne({ username: username });
         if (!findUser) {
-            return res.status(400).json({message:"Invalid username or password"});
+            return res.status(400).json({ message: "Invalid username or password" });
         }
-        
+
         const hashPassword = bcrypt.compare(password, findUser?.password);
         if (!hashPassword) {
-            return res.status(400).json({message:"Invalid username or password"});
+            return res.status(400).json({ message: "Invalid username or password" });
         }
 
         if (!process.env.JWT_SECRET) {
-            return console.log("JWT_SECRET required");   
+            return console.log("JWT_SECRET required");
         }
 
-        const token = jwt.sign(findUser.id, process.env.JWT_SECRET);
+        const userId = findUser._id.toString();
+        const token = jwt.sign(userId, process.env.JWT_SECRET);
 
-        res.status(200).json({message:"You are login successfully", token});
+        res.cookie('token', token, {
+            httpOnly: true,       // Prevent access via JavaScript
+            secure: true,         // Send only over HTTPS
+            sameSite: 'strict',   // Prevent CSRF
+        });
+        res.status(200).json({ message: "You are login successfully" });
     } catch (error) {
         console.error("Server-side error:", error);
-        res.status(500).json({ message: "Server-side error", error});
+        res.status(500).json({ message: "Server-side error", error });
     }
 });
 
+app.use((req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        res.status(400).json({ message: "You are not login" });
+    }
+
+    if (!process.env.JWT_SECRET) {
+        return console.log("JWT_SECRET required");
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+
+        if (decoded) {
+            req.userID = decoded.userId;
+            next();
+        }
+    } catch (error) {
+        console.error('Invalid token:', error);
+        res.status(400).json({ message: "Invalid token" });
+    }
 
 
-app.post('/content', () => {
+})
+
+
+
+app.post('/content', async (req, res): Promise<any> => {
+    const userId = req.userID;
+    const { title, url, tags, } = req.body;
+    if (!Array.isArray(tags) || !title || !url) {
+        return res.status(400).json({ message: "Invalid input data" });
+    }
+
+    const tagsArray = [];
+
+    try {
+        for (const tag of tags) {
+            const findTag = await TagtModel.findOneAndUpdate(
+                { title: tag },
+                { $setOnInsert: { title: tag } }, // Ensure the tag is set on insert
+                { new: true, upsert: true }      // Create a new document if not found
+            )
+            tagsArray.push(findTag._id);
+        }
+
+        await ContentModel.create({
+            url: url,
+            title: title,
+            userId: userId,
+            tags: tagsArray
+        })
+        res.status(200).json({ message: "Content added successfully" });
+    } catch (error) {
+        console.error("Server-side error:", error);
+        res.status(500).json({ message: "Server side error", error });
+    }
+});
+
+app.get('/content', async (req, res) => {
+    const userId = req.userID;
+
+    try {
+        const allContent = await ContentModel.find({ userId: userId });
+        res.status(200).json({allContent});
+    } catch (error) {
+        console.error("Server-side error:", error);
+        res.status(500).json({ message: "Server side error", error });
+    }
 
 });
 
-app.get('/content', () => {
-
-});
-
-app.post('/signup', () => {
-
-});
 
 
 async function main() {
