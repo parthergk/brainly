c199bd2b7400621553fc8de085df7f76cea54c9e
gaurav5xy <gauravkumar81464@gmail.com>
2025-01-23 23:52:16 +0530

    added button

---
+++ brainly-frontend/components/icons/index.ts
@@ -0,0 +1,9 @@
+export interface IconProps {
+    size: "sm" | "md" | "lg";
+}
+
+export const sizeVariant = {
+    "sm": 'size-2',
+    "md": 'size-6',
+    "lg": 'size-6'
+}
--- brainly-frontend/components/ui/Button.tsx
+++ brainly-frontend/components/ui/Button.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import { ReactElement } from "react";
 
 export interface ButtonProps {
   type: "primary" | "secondary";
   size: "sm" | "md" | "lg";
-  startIcon?: string;
+  startIcon?: ReactElement;
   text: string;
   onclick: () => void;
 }
 
-const veriant = {
-    "primary": "bg-purpole-600 text-white",
-    "secondary": "bg-purpole-300 text-purpole-300"
+const veriantStyle = {
+    "primary": "bg-purple-700 text-white",
+    "secondary": "bg-purple-300 text-purple-600"
 }
+
+const sizeStyle = {
+    "sm": "py-1 px-2",
+    "md": "py-2 px-4",
+    "lg": "py-3 px-6",
+}
+
+const customStyle =  'rounded-sm flex item-center justify-center gap-2'
 
 export const Button = (props: ButtonProps) => {
-    return <button className={veriant[props.type]}>{props.text}</button>
+    return <button  className={`${customStyle} ${veriantStyle[props.type]} ${sizeStyle[props.size]}`}> {props.startIcon} {props.text}</button>
 }
-
-<Button type="primary" size="md" text="hy" onclick={()=>{}}></Button>
