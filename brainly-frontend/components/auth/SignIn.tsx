33bc001216e50699931de664a4e1db8354460648
gaurav5xy <gauravkumar81464@gmail.com>
2025-01-26 00:02:09 +0530

    signin and signup page

---
+++ brainly-frontend/components/auth/SignIn.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Button } from "../ui/Button";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface InputsAuth{
+  username: string;
+  password: string 
+}
+
+
+const SignIn = () => {
+  const {register, handleSubmit, formState: {errors}} = useForm<InputsAuth>();
+
+  const onSubmit:SubmitHandler<InputsAuth> = (data)=>{
+    console.log(data);
+    
+  }
+
+  const handleKeyDown = (e:React.KeyboardEvent)=>{
+    if (e.key=="enter") {
+      e.preventDefault()
+    }
+  }
+
+  return (
+    <div className="bg-[#2c2c2c] text-white w-screen h-screen flex justify-center items-center p-4">
+      <div className=" bg-neutral-700/50 max-w-96 w-full rounded-sm p-4 border border-neutral-500">
+        <h1 className=" text-2xl text-center font-semibold">Sign In</h1>
+        <form onSubmit={handleSubmit(onSubmit)} onKeyDown={handleKeyDown}>
+          <input
+            {...register("username", { required: true })}
+            type="text"
+            placeholder="Username"
+            className=" w-full placeholder:text-neutral-400 outline-0 border border-neutral-300 px-2 py-1 mt-5"
+          />
+          {errors.username && (
+            <span className=" text-red-500 text-sm">
+              This field is required
+            </span>
+          )}
+          <input
+            {...register("password", { required: true })}
+            type="text"
+            placeholder="Password"
+            className=" w-full placeholder:text-neutral-400 outline-0 border border-neutral-300 px-2 py-1 mt-5"
+          />
+          {errors.password && (
+            <span className=" text-red-500 text-sm">
+              This field is required
+            </span>
+          )}
+          <div className=" w-full text-end mt-5 flex justify-end items-center">
+            <Button title="SignIn" type="secondary" size="md" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
--- brainly-frontend/src/main.tsx
+++ brainly-frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import SignIn from "../components/auth/SignIn.tsx";
+import SignUp from "../components/auth/SignUp.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+    </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
