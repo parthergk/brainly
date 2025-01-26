9b2fd9ed04d55b8623b10902538eb7d8ff8b6864
gaurav5xy <gauravkumar81464@gmail.com>
2025-01-26 22:08:54 +0530

    all route connected

--- brainly-frontend/components/TopBar.tsx
+++ brainly-frontend/components/TopBar.tsx
@@ -1,18 +1,61 @@
 import { Button } from "./ui/Button";
-import React from "react";
+import React, { useState } from "react";
 
-interface TopProps{
-  onClose: (a:boolean)=>void
+interface TopProps {
+  onClose: (a: boolean) => void;
 }
 
-const TopBar:React.FC<TopProps> = ({onClose}) => {
+const TopBar: React.FC<TopProps> = ({ onClose }) => {
+  const [message, setMessage] = useState<string | null>(null);
+  const [errMessage, setErrMessage] = useState<string | null>(null);
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const handleShare = async () => {
+    const response = await fetch("http://localhost:3000/brain/share", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        share: true,
+      }),
+    });
+    setIsOpen(true);
+    if (response.ok) {
+      const result = await response.json();
+      console.log("result", result);
+      
+      setMessage(result.hash);
+    } else {
+      const result = await response.json();
+      setErrMessage(result.message);
+    }
+  };
+
   return (
     <div className=" w-full h-16 flex justify-between items-center px-5 py-2 border-b border-neutral-600">
       <h1>Logo</h1>
       <div className=" flex gap-5">
-        <Button title="share" size="md" type="primary" onClick={()=>console.log("hello")
-        } />
-        <Button title="add" size="md" type="secondary" onClick={(()=>onClose(true))} />
+        <Button title="share" size="md" type="primary" onClick={handleShare} />
+        <Button
+          title="add"
+          size="md"
+          type="secondary"
+          onClick={() => onClose(true)}
+        />
+        {isOpen && <div className=" bg-white text-black fixed top-16 right-5 rounded-md min-w-60 p-2">
+          <div className=" flex justify-between mb-2">
+          <h1 className=" text-center shadow-xl px-1">share Link</h1>
+          <h1 className=" text-center shadow-xl px-1 cursor-pointer" onClick={()=>setIsOpen(false)}>close</h1>
+          </div>
+          {
+            message && <a href={`http://localhost:3000/brain/${message}`} target="_blank" className=" underline text-teal-600">http://localhost:3000/brain/{message}</a>
+          }
+          {
+          errMessage && <span className=" underline text-teal-600">{errMessage}</span>
+          }
+        </div>}
       </div>
     </div>
   );
